ae97becc4bd1a2dcc59b08ef1e0a3d54adcf10ea
Zakharia <49744677+zria37@users.noreply.github.com>
2024-06-25 15:02:20 +0700

    home data

--- HotelReservationSystem/obj/Debug/net8.0/HotelReservationSystem.AssemblyInfo.cs
+++ HotelReservationSystem/obj/Debug/net8.0/HotelReservationSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3552c6c2e22d3b959c7b1153e41bc78f83d228b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4f79c1d024aeda504e70b6686c061c1b5b0bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
