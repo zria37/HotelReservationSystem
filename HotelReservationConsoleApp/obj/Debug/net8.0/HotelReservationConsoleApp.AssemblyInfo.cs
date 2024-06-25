c6373ac9f6c351a3cde4f95e6810987e8d2d4831
faridmuzayyani <faridmuzayyani@gmail.com>
2024-06-26 04:20:39 +0700

    tesing

--- HotelReservationConsoleApp/obj/Debug/net8.0/HotelReservationConsoleApp.AssemblyInfo.cs
+++ HotelReservationConsoleApp/obj/Debug/net8.0/HotelReservationConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4f79c1d024aeda504e70b6686c061c1b5b0bf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a304e0f43b35ee8432b7cb8fc392d0b3d82e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HotelReservationSystem/Controllers/TestingController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelReservationSystem.Controllers
+{
+	public class TestingController : Controller
+	{
+		// GET: TestingController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: TestingController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: TestingController/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: TestingController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: TestingController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: TestingController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: TestingController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: TestingController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
